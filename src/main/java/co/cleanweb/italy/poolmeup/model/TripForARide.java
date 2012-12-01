ae65bd8e3091dd82cda2edbd0a58684606d3b7b3
Daniele <Daniele@10.25.3.252>
2012-12-01 22:23:59 +0100

    Aggiunta classe TripForARide (me l'ero scordata!).

---
+++ src/main/java/co/cleanweb/italy/poolmeup/model/TripForARide.java
@@ -0,0 +1,26 @@
+package co.cleanweb.italy.poolmeup.model;
+
+import co.cleanweb.italy.poolmeup.model.transport.Vehicle_Type;
+
+public class TripForARide {
+
+	protected String offererName;
+	protected String offererPhoneNumber;
+	protected Vehicle_Type vehicleType;
+	//protected Double waitingTime;
+
+	public TripForARide(String userName, String phoneNumber, Vehicle_Type vehicleType) {
+		this.offererName = userName;
+		this.offererPhoneNumber = phoneNumber;
+		this.vehicleType = vehicleType;
+	}
+	
+	public TripForARide(Trip trip) {
+		
+		this.offererName = trip.getDriverName();
+		this.offererPhoneNumber = trip.getDriverPhoneNumber();
+		this.vehicleType = trip.getVehicleType();
+		
+	}
+	
+}
