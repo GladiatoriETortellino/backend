605b31f02a96aa37705190827339873574d44ef6
Daniele <Daniele@10.25.3.252>
2012-12-01 17:58:42 +0100

    Classe Ride e RideRequest. Correzione lat lon da Long a Double.

---
+++ src/main/java/co/cleanweb/italy/poolmeup/model/transport/RideRequest.java
@@ -0,0 +1,10 @@
+package co.cleanweb.italy.poolmeup.model.transport;
+
+public class RideRequest {
+	
+	protected Long userID;
+	protected String userName;
+	protected Long numberOfPerson; 
+	//protected List<Double>
+
+}
