fd387d8b4a7c8395a11ecc50c52253d85eaa3ea3
Michele Orsi <morsi@maptoapp.com>
2012-12-01 23:06:52 +0100

    fix constructor

--- src/main/java/co/cleanweb/italy/poolmeup/model/Offer.java
+++ src/main/java/co/cleanweb/italy/poolmeup/model/Offer.java
@@ -38,7 +38,6 @@ public class Offer extends AbstractObjectPersist {
 	
 	private List<StepRequest> pathRequest = null;
 
-
 	//CONSTRUCTORS
 	public Offer() {}
 	
@@ -67,17 +66,116 @@ public class Offer extends AbstractObjectPersist {
 		this.pathRequest = offerRequest.getPathRequest();
 	}
 	//METHODS
+
+	/**
+	 * @return the phoneNumber
+	 */
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	/**
+	 * @param phoneNumber the phoneNumber to set
+	 */
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * @return the vehicleType
+	 */
+	public Vehicle_Type getVehicleType() {
+		return vehicleType;
+	}
+
+	/**
+	 * @param vehicleType the vehicleType to set
+	 */
+	public void setVehicleType(Vehicle_Type vehicleType) {
+		this.vehicleType = vehicleType;
+	}
+
 	/**
 	 * @return the numberPlaces
 	 */
 	public Integer getNumberPlaces() {
 		return numberPlaces;
 	}
+
 	/**
 	 * @param numberPlaces the numberPlaces to set
 	 */
 	public void setNumberPlaces(Integer numberPlaces) {
 		this.numberPlaces = numberPlaces;
 	}
-	
+
+	/**
+	 * @return the requestTime
+	 */
+	public Calendar getRequestTime() {
+		return requestTime;
+	}
+
+	/**
+	 * @param requestTime the requestTime to set
+	 */
+	public void setRequestTime(Calendar requestTime) {
+		this.requestTime = requestTime;
+	}
+
+	/**
+	 * @return the isStartingTime
+	 */
+	public Boolean getIsStartingTime() {
+		return isStartingTime;
+	}
+
+	/**
+	 * @param isStartingTime the isStartingTime to set
+	 */
+	public void setIsStartingTime(Boolean isStartingTime) {
+		this.isStartingTime = isStartingTime;
+	}
+
+	/**
+	 * @return the maxThreshold
+	 */
+	public Integer getMaxThreshold() {
+		return maxThreshold;
+	}
+
+	/**
+	 * @param maxThreshold the maxThreshold to set
+	 */
+	public void setMaxThreshold(Integer maxThreshold) {
+		this.maxThreshold = maxThreshold;
+	}
+
+	/**
+	 * @return the pathRequest
+	 */
+	public List<StepRequest> getPathRequest() {
+		return pathRequest;
+	}
+
+	/**
+	 * @param pathRequest the pathRequest to set
+	 */
+	public void setPathRequest(List<StepRequest> pathRequest) {
+		this.pathRequest = pathRequest;
+	}
 }
--- src/main/java/co/cleanweb/italy/poolmeup/model/TripForARide.java
+++ src/main/java/co/cleanweb/italy/poolmeup/model/TripForARide.java
@@ -24,4 +24,6 @@ public class TripForARide {
 		
 	}
 	
+	public TripForARide() {}
+	
 }
