ec3d2311310a974e8a7afe5b5fd5bfca43965d1f
Michele Orsi <morsi@maptoapp.com>
2012-12-01 15:05:56 +0100

    added all maven config

---
+++ src/main/java/co/cleanweb/italy/poolmeup/testClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package co.cleanweb.italy.poolmeup;
+
+/**
+ * @author micheleorsi
+ *
+ */
+public class testClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.print("Hello world");
+
+	}
+
+}
